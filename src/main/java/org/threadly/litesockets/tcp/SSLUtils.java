8a51b21591022094ffb7e5e160aa47734ecda048
Luke Wahlmeier <lwahlmeier@gmail.com>
2015-05-13 12:45:55 -0600

    removed some unused methods, updated a unit test to check ssl state

--- src/main/java/org/threadly/litesockets/tcp/SSLUtils.java
+++ src/main/java/org/threadly/litesockets/tcp/SSLUtils.java
@@ -1,17 +1,11 @@
 package org.threadly.litesockets.tcp;
 
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLEngine;
-import javax.net.ssl.SSLEngineResult;
-import javax.net.ssl.SSLHandshakeException;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
@@ -88,66 +82,4 @@ public class SSLUtils {
       return new X509Certificate[0];
     }
   }
-  
-  /**
-   * This is a helper function used to complete the SSLHandshake internally.  This is what we do when 
-   * the HandshakeStatus is NEED_UNWRAP, which basically means need Read.
-   * 
-   * @param networkDataBuffer this is the networkBuffer that was last read off the wire. 
-   * @param peerData This is the buffer to write into after we 
-   * @param ssle the SSLEngine used in the handshake.
-   * @param channel the socketChannel the handshake is happening on.
-   * @return the current HandshalStatus
-   * @throws IOException this will happen if there is any problems decrypting the data or reading/writing to the socket.
-   */
-  public static SSLEngineResult.HandshakeStatus doHandShakeRead(ByteBuffer networkDataBuffer, 
-      ByteBuffer peerData, SSLEngine ssle, SocketChannel channel) throws IOException {
-    peerData.clear();
-    SSLEngineResult.HandshakeStatus hs;
-    if (channel.read(networkDataBuffer) < 0) {
-      //Got close
-      throw new SSLHandshakeException(SSL_HANDSHAKE_ERROR);
-    }
-    networkDataBuffer.flip();
-    SSLEngineResult res = ssle.unwrap(networkDataBuffer, peerData);
-    networkDataBuffer.compact();
-    hs = res.getHandshakeStatus();
-    if(res.getStatus() != SSLEngineResult.Status.OK  && 
-        res.getStatus() != SSLEngineResult.Status.BUFFER_UNDERFLOW) {
-      throw new SSLHandshakeException(SSL_HANDSHAKE_ERROR+":"+res.getStatus());
-    }
-    return hs;
-  } 
-  
-  /**
-   * This is a helper function used to complete the SSLHandshake internally.  This is what 
-   * we do when the HandshakeStatus is NEED_WRAP, which basically means need write.
-   *
-   * @param appBuffer This is the buffer that is encrypted.  It should essentially be empty as wrap will add the handshake data needed to it. 
-   * @param networkData this is the buffer that will store the encrypted data and be written to the socket. 
-   * @param ssle the SSLEngine used in the handshake.
-   * @param channel the socketChannel the handshake is happening on.
-   * @return the current HandshalStatus
-   * @throws IOException this will happen if there is any problems decrypting the data or reading/writing to the socket.
-   */
-  public static SSLEngineResult.HandshakeStatus doHandShakeWrite(ByteBuffer appBuffer, ByteBuffer networkData, 
-      SSLEngine ssle, SocketChannel channel) throws IOException {
-    networkData.clear();
-    SSLEngineResult.HandshakeStatus hs;
-    SSLEngineResult res = ssle.wrap(appBuffer, networkData);
-    hs = res.getHandshakeStatus();
-    
-    if(res.getStatus() == SSLEngineResult.Status.OK) {
-      networkData.flip();
-      while (networkData.hasRemaining()) {
-        if (channel.write(networkData) < 0) {
-          throw new SSLHandshakeException(SSL_HANDSHAKE_ERROR);
-        }
-      }
-    } else {
-      throw new SSLHandshakeException(SSL_HANDSHAKE_ERROR+":"+res.getStatus());
-    }
-    return hs;
-  }
-
 }
